cf3cf539258e12217fa9399e77b7476f5289f6e0
Jibbyworks <JDBucog@yahoo.com>
2021-09-21 23:30:55 -0700

    set up useReducer

--- src/CartContainer.js
+++ src/CartContainer.js
@@ -1,22 +1,22 @@
-import React from 'react'
-import CartItem from './CartItem'
-import { useGlobalContext } from './context'
+import React from "react"
+import CartItem from "./CartItem"
+import { useGlobalContext } from "./context"
 
 const CartContainer = () => {
-  const { cart } = useGlobalContext()
+  const { cart, total } = useGlobalContext()
   if (cart.length === 0) {
     return (
-      <section className='cart'>
+      <section className="cart">
         {/* cart header */}
         <header>
           <h2>your bag</h2>
-          <h4 className='empty-cart'>is currently empty</h4>
+          <h4 className="empty-cart">is currently empty</h4>
         </header>
       </section>
     )
   }
   return (
-    <section className='cart'>
+    <section className="cart">
       {/* cart header */}
       <header>
         <h2>your bag</h2>
@@ -30,14 +30,14 @@ const CartContainer = () => {
       {/* cart footer */}
       <footer>
         <hr />
-        <div className='cart-total'>
+        <div className="cart-total">
           <h4>
-            total <span>$0.00</span>
+            total <span>{total}</span>
           </h4>
         </div>
         <button
-          className='btn clear-btn'
-          onClick={() => console.log('clear cart')}
+          className="btn clear-btn"
+          onClick={() => console.log("clear cart")}
         >
           clear cart
         </button>
